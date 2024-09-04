e63f8bb115ae30b56c91acaa9fa84c8596afee5c
mohamuhsin <muhsinohoho@outlook.com>
2024-09-04 16:51:33 +0300

    did clean up

--- src/store/index.js
+++ src/store/index.js
@@ -21,38 +21,6 @@ const counterSlice = createSlice({
     },
 });
 
-const counterReducer = (state = initialState, action) => {
-    if (action.type === "increment") {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === "decrement") {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === "increase") {
-        return {
-            counter: state.counter + action.amount,
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === "toggle") {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter,
-        };
-    }
-
-    return state;
-};
-
 const store = configureStore({
     reducer: counterSlice.reducer,
 });
